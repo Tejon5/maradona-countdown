136f4c06dfc6bd0a065f04c9b05692eb16ec3e39
Tejon5 <juan.villalba08@gmail.com>
2025-02-03 00:00:05 -0300

    vs Racing 2025

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ const cuantoescounter = (utc2, utc3) => {
 obtenerPais2(utc => {
     let utc2 = Number(utc)  
     cuantoescounter(utc2,utc3)
-    let countDate = new Date("Feb 02 2025 19:15:00").getTime()/*+utc2*/;/**17'30 */
+    let countDate = new Date("Feb 08 2025 21:30:00").getTime()/*+utc2*/;/**17'30 */
     countdown(countDate,result,state)
 });
 
