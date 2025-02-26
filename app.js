f6048861b8f3ebecc67a62883b2971b7d85adfa5
Tejon5 <juan.villalba08@gmail.com>
2025-02-26 09:23:26 -0300

    vs rosario central

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ const cuantoescounter = (utc2, utc3) => {
 obtenerPais2(utc => {
     let utc2 = Number(utc)  
     cuantoescounter(utc2,utc3)
-    let countDate = new Date("Feb 25 2025 21:30:00").getTime()/*+utc2*/;/**17'30 */
+    let countDate = new Date("Feb 28 2025 20:00:00").getTime()/*+utc2*/;/**17'30 */
     countdown(countDate,result,state)
 });
 
