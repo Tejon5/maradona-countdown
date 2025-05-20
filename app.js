21e3bc6783b097f8d602f9f976068ce2b6f71f81
Tejon5 <juan.villalba08@gmail.com>
2025-05-20 09:16:09 -0300

    Copa Argentina

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ const cuantoescounter = (utc2, utc3) => {
 obtenerPais2(utc => {
     let utc2 = Number(utc)  
     cuantoescounter(utc2,utc3)
-    let countDate = new Date("May 19 2025 21:30:00").getTime()/*+utc2*/;/**17'30 */
+    let countDate = new Date("Jun 07 2025 21:00:00").getTime()/*+utc2*/;/**17'30 */
     countdown(countDate,result,state)
 });
 
