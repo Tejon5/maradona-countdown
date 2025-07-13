724c230a23b58377978c9bca3d3049d5048a470f
Juan <juan.villalba08@gmail.com>
2025-07-13 20:54:21 -0300

    vs union

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ const cuantoescounter = (utc2, utc3) => {
 obtenerPais2(utc => {
     let utc2 = Number(utc)  
     cuantoescounter(utc2,utc3)
-    let countDate = new Date("Jul 13 2025 18:45:00").getTime()/*+utc2*/;/**17'30 */
+    let countDate = new Date("Jul 18 2025 19:30:00").getTime()/*+utc2*/;/**17'30 */
     countdown(countDate,result,state)
 });
 
