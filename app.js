92205169b0c9e09206b15a3be670c3a7d6e3b32f
Juan <juan.villalba08@gmail.com>
2025-10-05 20:33:35 -0300

    vs barracas

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ const cuantoescounter = (utc2, utc3) => {
 obtenerPais2(utc => {
     let utc2 = Number(utc)  
     cuantoescounter(utc2,utc3)
-    let countDate = new Date("Oct 05 2025 19:00:00").getTime()/*+utc2*/;/**17'30 */
+    let countDate = new Date("Oct 11 2025 14:30:00").getTime()/*+utc2*/;/**17'30 */
     countdown(countDate,result,state)
 });
 
