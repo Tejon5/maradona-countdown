7c67bd0d3993230e9a4cb9103840f4f4f1af0aa1
Juan <juan.villalba08@gmail.com>
2025-11-09 18:49:30 -0300

    vs tigre

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ const cuantoescounter = (utc2, utc3) => {
 obtenerPais2(utc => {
     let utc2 = Number(utc)  
     cuantoescounter(utc2,utc3)
-    let countDate = new Date("Nov 09 2025 16:30:00").getTime()/*+utc2*/;/**17'30 */
+    let countDate = new Date("Nov 16 2025 14:00:00").getTime()/*+utc2*/;/**17'30 */
     countdown(countDate,result,state)
 });
 
