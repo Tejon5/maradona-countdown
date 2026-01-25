bb676194ccbe402eb837562ae8fc05eb33d322c0
Juan <juan.villalba08@gmail.com>
2026-01-25 20:50:08 -0300

    vs Estudiantes

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ const cuantoescounter = (utc2, utc3) => {
 obtenerPais2(utc => {
     let utc2 = Number(utc)  
     cuantoescounter(utc2,utc3)
-    let countDate = new Date("Jan 25 2026 18:30:00").getTime()/*+utc2*/;/**17'30 */
+    let countDate = new Date("Jan 28 2026 22:15:00").getTime()/*+utc2*/;/**17'30 */
     countdown(countDate,result,state)
 });
 
