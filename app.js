9b7a60f5d152d96d5d5fa2b4a8356b7de6ba86e7
Juan <juan.villalba08@gmail.com>
2026-02-28 19:27:13 -0300

    vs Lanus

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ const cuantoescounter = (utc2, utc3) => {
 obtenerPais2(utc => {
     let utc2 = Number(utc)  
     cuantoescounter(utc2,utc3)
-    let countDate = new Date("Feb 28 2026 17:45:00").getTime()/*+utc2*/;/**17'30 */
+    let countDate = new Date("Mar 04 2026 21:00:00").getTime()/*+utc2*/;/**17'30 */
     countdown(countDate,result,state)
 });
 
